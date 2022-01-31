6438fbc6f5a020eed2771ec1a5dbd7f75c91346b
Viktor <mec.viktor10@gmail.com>
2022-01-31 22:49:46 +0100

    js redirect based on os

---
+++ script.js
@@ -0,0 +1,4 @@
+if (navigator.userAgent.indexOf("Android") != -1)
+  window.location.replace("https://play.google.com/");
+if (navigator.userAgent.indexOf("like Mac") != -1)
+  window.location.replace("https://apps.apple.com/");
