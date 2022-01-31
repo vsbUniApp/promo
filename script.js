573108949a8dd9b29948745689ffc3c2df5c0ed6
Viktor <mec.viktor10@gmail.com>
2022-01-31 23:05:09 +0100

    changed to separate files

--- script.js
+++
@@ -1,4 +0,0 @@
-if (navigator.userAgent.indexOf("Android") != -1)
-  window.location.href = "https://play.google.com/";
-if (navigator.userAgent.indexOf("like Mac") != -1)
-  window.location.href = "https://apps.apple.com/";
